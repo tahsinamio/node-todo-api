6f299c850ceba01730b39f767956e3ce93060a55
Tahsin Wasit Amio <tahsinamio@gmail.com>
2018-05-24 12:56:30 +0600

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -31,6 +31,24 @@ app.get('/todos', (req, res) => {
   });
 });
 
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectId.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+   }).catch((e) => {
+     res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up at port ${port}`);
 });
