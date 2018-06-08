8eb264af261f5175d77fc21f376a58ebceb29bef
Tahsin Wasit Amio <tahsinamio@gmail.com>
2018-06-08 12:15:42 +0600

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -97,7 +97,6 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
-// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -113,6 +112,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
