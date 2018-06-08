5487b444974e99c384bda6562628b2fa9659463c
Tahsin Wasit Amio <tahsinamio@gmail.com>
2018-06-08 15:06:29 +0600

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -97,6 +97,7 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+// POST /users
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
@@ -122,6 +123,14 @@ app.post('/users/login', (req, res) => {
       res.header('x-auth', token).send(user);
     });
   }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
     res.status(400).send();
   });
 });
